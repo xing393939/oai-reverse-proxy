ad0a3c09362acf57e982e0cb9805f86436d825b5
nai-degen <khoners@protonmail.com>
2024-10-09 16:32:48 -0500

    removes cors/sec-fetch headers to fix venus chub with anthropic api

--- src/proxy/middleware/request/mutators/strip-headers.ts
+++ src/proxy/middleware/request/mutators/strip-headers.ts
@@ -7,11 +7,23 @@ import { ProxyReqMutator } from "../index";
 export const stripHeaders: ProxyReqMutator = (manager) => {
   manager.setHeader("origin", "");
   manager.setHeader("referer", "");
+
+  // Some APIs refuse requests coming from browsers to discourage embedding
+  // API keys in client-side code, so we must remove all CORS/fetch headers.
+  Object.keys(manager.request.headers).forEach((key) => {
+    if (key.startsWith("sec-")) {
+      manager.removeHeader(key);
+    }
+  });
+
   manager.removeHeader("tailscale-user-login");
   manager.removeHeader("tailscale-user-name");
   manager.removeHeader("tailscale-headers-info");
   manager.removeHeader("tailscale-user-profile-pic");
   manager.removeHeader("cf-connecting-ip");
+  manager.removeHeader("cf-ray");
+  manager.removeHeader("cf-visitor");
+  manager.removeHeader("cf-warp-tag-id");
   manager.removeHeader("forwarded");
   manager.removeHeader("true-client-ip");
   manager.removeHeader("x-forwarded-for");
