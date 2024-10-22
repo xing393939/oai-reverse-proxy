4190d5fef663df4a233e2fd969a1c16f664599b9
khanon <khoners@protonmail.com>
2024-10-22 16:06:39 +0000

    fixes missing comma.......

--- src/proxy/anthropic.ts
+++ src/proxy/anthropic.ts
@@ -37,6 +37,7 @@ const getModelsResponse = () => {
     "claude-2.1",
     "claude-3-haiku-20240307",
     "claude-3-opus-20240229",
+    "claude-3-opus-latest",
     "claude-3-sonnet-20240229",
     "claude-3-5-sonnet-20240620",
     "claude-3-5-sonnet-20241022",
